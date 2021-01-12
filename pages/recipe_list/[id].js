040b33f0f32f38ec864f56fcbcbd282dc9d13bbf
TwistZA <avi3110@gmail.com>
2021-01-12 23:31:32 +0200

    Added Links to homepage to route to recipe lists. Minor Style changes

---
+++ pages/recipe_list/[id].js
@@ -0,0 +1,56 @@
+import Layout from "../../src/components/Layout/Layout";
+import styles from "./RecipeList.module.css";
+
+const getRecipeList = async (id) => {
+  const res = await fetch(
+    `https://www.themealdb.com/api/json/v1/1/filter.php?c=${id}`
+  );
+
+  const recipe_list = await res.json();
+
+  return recipe_list.meals;
+};
+
+export default function RecipeList({ recipe_list }) {
+  return (
+    <div className={styles.meals_container}>
+      {recipe_list.map((recipe) => (
+        <div className={styles.card} key={recipe.idMeal}>
+          <img src={recipe.strMealThumb} alt={recipe.strMeal}></img>
+          <div className={styles.recipe_button}>
+            <a href="#">
+              <h3>{recipe.strMeal}</h3>
+              <button className={styles.recipe_button_inner}>Recipe</button>
+            </a>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export const getStaticPaths = async () => {
+  const res = await fetch(
+    "https://www.themealdb.com/api/json/v1/1/categories.php"
+  );
+  const categories = await res.json();
+
+  console.log("--------getStaticPaths-------");
+  const paths = categories.categories.map((category) => ({
+    params: { id: category.strCategory },
+  }));
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async ({ params }) => {
+  const recipe_list = await getRecipeList(params.id);
+  console.log("--------getStaticProps-------");
+  return {
+    props: {
+      recipe_list,
+    },
+  };
+};
--- src/components/MealsTable/MealsTable.js
+++ src/components/MealsTable/MealsTable.js
@@ -1,18 +1,27 @@
 import React from "react";
 import styles from "./MealsTable.module.css";
+import Link from "next/link";
 
 const MealsTable = ({ categories }) => {
   return (
     <div className={styles.meals_container}>
       {categories.categories.map((category) => (
-        <div className={styles.card} key={category.strCategory}>
-          <img src={category.strCategoryThumb} alt={category.strCategory}></img>
-          <div className={styles.recipe_button}>
-            <a href="#">
-              <h1>{category.strCategory} Recipes</h1>
-            </a>
+        <Link
+          href={`/recipe_list/${category.strCategory}`}
+          key={category.strCategory}
+        >
+          <div className={styles.card} key={category.strCategory}>
+            <img
+              src={category.strCategoryThumb}
+              alt={category.strCategory}
+            ></img>
+            <div className={styles.recipe_button}>
+              <a href="#">
+                <h3>{category.strCategory} Recipes</h3>
+              </a>
+            </div>
           </div>
-        </div>
+        </Link>
       ))}
     </div>
   );
